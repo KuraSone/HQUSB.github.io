7df68a868341c644bed2e34ee2905c24d7943c93
Kura <61506443+KuraSone@users.noreply.github.com>
2022-11-15 10:56:19 +0800

    Update js.js

--- 请假v3/script/js.js
+++ 请假v3/script/js.js
@@ -2,3 +2,5 @@ let date = new Date()
 let td = new Date()
 
 $id('yjfxcfsj2').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDate())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
+
+$id('yjfxddsj2').innerHTML = `${zero(td.getMonth()+1)}-${zero(td.getDate())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
